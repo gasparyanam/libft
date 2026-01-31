a409a650420728685e130c9a8f2161b44409a133
Arman Gasparyan <agaspary@c2r6s3.42yerevan.am>
2026-01-31 20:23:21 +0400

    strlcpy strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,13 +6,12 @@
 /*   By: agaspary <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/31 16:34:10 by agaspary          #+#    #+#             */
-/*   Updated: 2026/01/31 19:21:08 by agaspary         ###   ########.fr       */
+/*   Updated: 2026/01/31 20:01:10 by agaspary         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include "libft.h"
-#include <stdio.h>
-#include <stddef.h>
+#include "libft.h"
+
 char	*ft_strnstr(const char *haystack, const char *needle, size_t n)
 {
 	size_t	i;
@@ -33,23 +32,8 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t n)
 				j++;
 			}
 		}
+		j = 0;
 		i++;
 	}
 	return (NULL);
 }
-
-int main()
-{
-	const char *haystack = "Hello, this is a simpo example simpke.";
-	const char *needle = "simple";
-	size_t n = 50;
-
-	char *result = ft_strnstr(haystack, needle, n);
-	if (result)
-		printf("Found: %s\n", result);
-	else
-		printf("Not Found\n");
-
-	return 0;
-}
-
