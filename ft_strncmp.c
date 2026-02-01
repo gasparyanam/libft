d149da5926030af51bdde9d85beff899cabd365c
Arman Gasparyan <agaspary@c2r10s3.42yerevan.am>
2026-02-01 13:30:05 +0400

    new strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,18 +6,18 @@
 /*   By: agaspary <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/30 18:32:22 by agaspary          #+#    #+#             */
-/*   Updated: 2026/01/31 11:52:03 by agaspary         ###   ########.fr       */
+/*   Updated: 2026/02/01 13:29:38 by agaspary         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lift.h"
 
-int	ft_strncmp(const char *s1, const char *s2, size_t n)
+int	ft_strncmp(const char *s1, const char *s2, size_t num)
 {
 	size_t	i;
 
 	i = 0;
-	while (i < n)
+	while (i < num)
 	{
 		if ((unsigned char)s1[1] != (unsigned char)s2[i])
 			return ((unsigned char)s[i] - (unsigned char)s2[i]);
